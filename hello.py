a37e63002d33f9b38696fcdbf8e934304e6ddcd2
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-09-30 13:00:44 +0300

    data structures and algorithms under python

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("data structures and alogirithims")
+num1 = 20
+num2 = 20
+print(num1+num2)
+print(type(num2))
---
+++ recursion.py
@@ -0,0 +1,19 @@
+def one():
+    print(1)
+    two()
+    
+
+def two():
+    print(2)
+    three()
+   
+
+def three():
+    print(3)
+    four()
+    
+
+def four():
+    print(4)
+
+one()
