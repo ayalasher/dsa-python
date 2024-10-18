0c8ad1c18b02d994822100cb0bdbb219da4d9d6d
ayalasher <144620362+ayalasher@users.noreply.github.com>
2024-10-18 12:23:36 +0300

    covered arrays

---
+++ arrays.py
@@ -0,0 +1,19 @@
+from array import *
+
+
+ages = array("i",[2,3,4,6,7])
+
+print(ages)
+
+# end of the array
+ages.append(55)
+print(ages)
+
+
+#start of the array
+ages.insert(0,88)
+print(ages)
+
+# center of the array
+ages.insert(3,76)
+print(ages)
